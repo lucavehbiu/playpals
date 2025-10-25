e638c8dd5875e455f90e59294c2823e79be0814e
lucavehbiu <lucavehbiu96@gmail.com>
2025-10-25 14:27:01 +0200

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -1,3 +1,4 @@
+import "dotenv/config";
 import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
@@ -50,11 +51,7 @@ app.use((req, res, next) => {
   // Heroku dynamically assigns a port, so we must use the PORT environment variable.
   // Default to 5000 for local development if PORT is not set.
   const port = process.env.PORT || 5000;
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  server.listen(port, "0.0.0.0", () => {
     log(`serving on port ${port}`);
   });
 })();
